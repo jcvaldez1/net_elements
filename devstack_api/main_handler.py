ce8baf88989676fe3bc70813f29369c061a030df
jcvaldez1 <jcvaldez1@up.edu.ph>
2019-05-06 23:49:52 +0800

    modified for add all

--- devstack_api/main_handler.py
+++ devstack_api/main_handler.py
@@ -27,6 +27,8 @@ class main_handler():
         print("TOKEN RETRIEVAL DONE")
 
         self.public_access_ip = None
+        self.private_network_id = None
+        self.public_network_id = None
 
     def load_json_file(self, filename):
         try:
@@ -43,37 +45,72 @@ class main_handler():
         except KeyError:
             raise KeyError(str(key) + " was not found")
 
-    def gen_default_json(self):
-        metadata = { "url":"http://10.147.4.69/compute/v2.1/servers",
-                     "name": "cirros server" }
-        server = self.gen_server_data()
+    def gen_default_json(self, server=None, metadata=None):
+        if not metadata:
+            metadata = { "url":"http://10.147.4.69/compute/v2.1/servers",
+                         "name": "cirros server" }
+        
+        if not server:
+            server = self.gen_server_data()
+        
         actual_data = {"server" : server}
         return { "actual_data" : actual_data, "metadata" : metadata }
 
     def gen_server_data(self):
         server_data = {"name" : "cirros server"}
         # GET IMAGES
-        resp = json.loads(message(json_data={}, headers=self.headers,
-                       url=config.IMAGES_URL).send_message("GET").text)
-        server_data["imageRef"] = resp["images"][0]["id"]
+        # resp = json.loads(message(json_data={}, headers=self.headers,
+        #                url=config.IMAGES_URL).send_message("GET").text)
+        # server_data["imageRef"] = resp["images"][0]["id"]
+        images = self.retrieve_images()
+        server_data["imageRef"] = images[0]["id"]
+        
         # GET FLAVORS
-        resp = json.loads(message(json_data={}, headers=self.headers,
-                       url=config.FLAVORS_URL).send_message("GET").text)
-        server_data["flavorRef"] = resp["flavors"][9]["id"]
+        # resp = json.loads(message(json_data={}, headers=self.headers,
+        #                url=config.FLAVORS_URL).send_message("GET").text)
+        # server_data["flavorRef"] = resp["flavors"][9]["id"]
+        flavors = self.retrieve_flavors()
+        server_data["flavorRef"] = flavors[9]["id"]
         # GET NETWORKS
+        
         uid = {}
-        resp = json.loads(message(json_data={}, headers=self.headers,
-                       url=config.NETWORKS_URL).send_message("GET").text)
-        for x in resp["networks"]:
-            if x["name"] == 'private':
-                uid["uuid"] = x["id"]
-            elif x["name"] == 'public':
-                self.public_access_ip = x["id"]
+        uid["uuid"], self.public_access_ip = self.retrieve_network_ids()
+        
+        # resp = json.loads(message(json_data={}, headers=self.headers,
+        #                url=config.NETWORKS_URL).send_message("GET").text)
+        # for x in resp["networks"]:
+        #     if x["name"] == 'private':
+        #         uid["uuid"] = x["id"]
+        #     elif x["name"] == 'public':
+        #         self.public_access_ip = x["id"]
 
 
         server_data["networks"] = [uid]
         #server_data["accessIPv4"] = self.set_floating(uid["uuid"])
         return server_data
+
+    # returns the private and public ids of the openstack network
+    def retrieve_flavors(self):
+
+        resp = json.loads(message(json_data={}, headers=self.headers,
+                       url=config.FLAVORS_URL).send_message("GET").text)
+        return resp["flavors"]
+
+    def retrieve_images(self):
+        
+        resp = json.loads(message(json_data={}, headers=self.headers,
+                       url=config.IMAGES_URL).send_message("GET").text)
+        return resp["images"]
+
+    def retrieve_network_ids(self):
+        
+        resp = json.loads(message(json_data={}, headers=self.headers, url=config.NETWORKS_URL).send_message("GET").text)
+        for x in resp["networks"]:
+            if x["name"] == 'private':
+                private_id = x["id"]
+            elif x["name"] == 'public':
+                public_id = x["id"]
+        return private_id, public_id
 
     def set_floating(self, the_id):
         #resp = json.loads(message(json_data={}, headers=self.headers,
@@ -133,10 +170,53 @@ class main_handler():
         port_id = self.get_port_id(json.loads(test.text)["server"]["id"])
         print("UPDATING PORT INFO")
         self.update_port_id(public_floatingip_id, port_id)
+    
+    def generate_server(self, imgref, flvref, srvname):
+        server_data = {"name" : srvname, "imageRef" : imgref, "flavorRef" : flvref}
+        server_data["networks"] = [ {"uuid" : self.private_network_id} ]
+        return server_data
+    
+    def write_json(self, name, data):
+        with open('/home/thesis/net_elements/devstack_api/jsonfiles/' + name + '.json',"w") as outfile:
+                json.dump( data, outfile, indent=4)
 
+    # Instantiates all of the images
+    def remote_up_all(self):
+        # retrieve network ids
+        self.private_network_id, self.public_network_id = self.retrieve_network_ids()
+
+        #retrive list of images and flavors
+        images = self.retrieve_images()
+        flavors = self.retrieve_flavors()
+
+        # retrieve list of instances
+        # instance sample:
+        # { img_name, flavor_name, instance_name, instance_ip}
+        instances = []
+        img_id = None
+        flavor_id = None
+        for inst in instances:
+            # retrieve img_id and flavor_id 
+            img_id = self.find_object_match(inst.img_name, "name", images)
+            flavor_id = self.find_object_match(inst.flavor_name, "name", flavors)
+            
+            inst_data = self.gen_default_json( self.generate_server( img_id, flavor_id, inst.instance_name ), {} )
+            self.write_json( inst.name, inst_data )
+            inst_object = object_class.an_object( the_type=self.load_json_file( inst.name ) )
+            message(json_data=json.dumps( inst_object.dump_dict() ), url=config.SERVERS_URL, headers=self.headers).send_message('POST')
+    
+    # finds object with val == object.attr for object in obj_list
+    def find_object_match(self, val, attr, obj_list):
+        for obj in obj_list:
+            if obj[attr] == val:
+                return obj
+        else:
+            return None   
 
 
     # add more API shit here in the future
+
+
 
 class message():
     def __init__(self, **kwargs):
